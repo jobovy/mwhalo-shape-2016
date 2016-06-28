44e5dcf84ecc2f8d947bfec300ea9b0b96a11dc8
Jo Bovy <bovy@astro.utoronto.ca>
2016-06-27 23:57:35 -0400

    More robust pal5 predictions, start on likelihood

--- py/pal5_util.py
+++ py/pal5_util.py
@@ -2,7 +2,7 @@ import copy
 import warnings
 import numpy
 from scipy import interpolate
-from galpy.actionAngle import actionAngleIsochroneApprox
+from galpy.actionAngle import actionAngleIsochroneApprox, estimateBIsochrone
 from galpy.orbit import Orbit
 from galpy.df import streamdf
 from galpy.util import bovy_conversion, bovy_coords
@@ -50,7 +50,7 @@ def predict_pal5obs(pot_params,c,
                     ro=_REFR0,vo=_REFV0,
                     singlec=False,
                     interpcs=None,interpk=None,
-                    nTrackChunks=8,multi=None):
+                    isob=None,nTrackChunks=8,multi=None):
     """
     NAME:
        predict_pal5obs
@@ -71,6 +71,7 @@ def predict_pal5obs(pot_params,c,
        interpcs= (None) values of c at which to compute the model for interpolation
        nTrackChunks= (8) nTrackChunks input to streamdf
        multi= (None) multi input to streamdf
+       isob= (None) if given, b parameter of actionAngleIsochroneApprox
     OUTPUT:
        (trailing track in RA,Dec,
         leading track in RA,Dec,
@@ -93,64 +94,45 @@ def predict_pal5obs(pot_params,c,
         sigv= [sigv for i in interpcs]
     if isinstance(td,float):
         td= [td for i in interpcs]
+    if isinstance(isob,float) or isob is None:
+        isob= [isob for i in interpcs]
+    prog= Orbit([229.018,-0.124,dist,pmra,pmdec,vlos],
+                radec=True,ro=ro,vo=vo,
+                solarmotion=[-11.1,24.,7.25])
     # Setup the model
     sdf_trailing_varyc= []
     sdf_leading_varyc= []
-    for ii,ic in enumerate(interpcs):
+    ii= 0
+    nt= 0
+    tries= [0.05,-0.05]
+    if len(interpcs) ==1 or interpcs[1]-interpcs[0] > 0.1:
+        tries.extend([0.1,-0.1])
+    origic= interpcs[0]
+    while ii < len(interpcs):
+        ic= interpcs[ii]
         pot= MWPotential2014Likelihood.setup_potential(pot_params,ic,
                                                        False,False,ro,vo)
-        prog= Orbit([229.018,-0.124,dist,pmra,pmdec,vlos],
-                    radec=True,ro=ro,vo=vo,
-                    solarmotion=[-11.1,24.,7.25])
-        aAI= actionAngleIsochroneApprox(pot=pot,b=0.8)
-        try:
-            tsdf_trailing=\
-                streamdf(sigv[ii]/vo,progenitor=prog,pot=pot,aA=aAI,
-                         leading=False,nTrackChunks=nTrackChunks,
-                         tdisrupt=td[ii]/bovy_conversion.time_in_Gyr(vo,ro),
-                         ro=ro,vo=vo,R0=ro,
-                         vsun=[-11.1,vo+24.,7.25],
-                         custom_transform=_TPAL5,
-                         multi=multi)
-        except numpy.linalg.LinAlgError:
-            tsdf_trailing=\
-                streamdf(sigv[ii]/vo,progenitor=prog,pot=pot,aA=aAI,
-                         leading=False,nTrackChunks=nTrackChunks,
-                         nTrackIterations=0,
-                         tdisrupt=td[ii]/bovy_conversion.time_in_Gyr(vo,ro),
-                         ro=ro,vo=vo,R0=ro,
-                         vsun=[-11.1,vo+24.,7.25],
-                         custom_transform=_TPAL5,
-                         multi=multi)
-        try:
-            tsdf_leading=\
-                streamdf(sigv[ii]/vo,progenitor=prog,pot=pot,aA=aAI,
-                         leading=True,nTrackChunks=nTrackChunks,
-                         tdisrupt=td[ii]/bovy_conversion.time_in_Gyr(vo,ro),
-                         ro=ro,vo=vo,R0=ro,
-                         vsun=[-11.1,vo+24.,7.25],
-                         custom_transform=_TPAL5,
-                         multi=multi)
-        except numpy.linalg.LinAlgError:
-            tsdf_leading=\
-                streamdf(sigv[ii]/vo,progenitor=prog,pot=pot,aA=aAI,
-                         leading=True,nTrackChunks=nTrackChunks,
-                         nTrackIterations=0,
-                         tdisrupt=td[ii]/bovy_conversion.time_in_Gyr(vo,ro),
-                         ro=ro,vo=vo,R0=ro,
-                         vsun=[-11.1,vo+24.,7.25],
-                         custom_transform=_TPAL5,
-                         multi=multi)
-        # Check that we're not too close to having a problem
-        if numpy.fabs(tsdf_trailing._dOdJpEig[0][2]\
-                          /tsdf_trailing._dOdJpEig[0][1]) < 0.05:
-            warnings.warn("Potential with c = %.1f close to one for which we expect there to be problems with the calculation" % ic)
-            if not singlec:
-                interpcs.remove(ic)
-                continue
-        # Add to the list
-        sdf_trailing_varyc.append(tsdf_trailing)
-        sdf_leading_varyc.append(tsdf_leading)
+        tsdf_trailing, tsdf_leading= setup_sdf(pot,prog,sigv[ii],td[ii],
+                                               ro,vo,multi=multi,isob=isob[ii],
+                                               nTrackChunks=nTrackChunks)
+        # Check for calc. issue: when track starts too high
+        if nt < len(tries) and (bovy_coords.lb_to_radec(\
+            tsdf_trailing._interpolatedObsTrackLB[0,0],
+            tsdf_trailing._interpolatedObsTrackLB[0,1],
+            degree=True)[1] > 0.625 or \
+            bovy_coords.lb_to_radec(\
+            tsdf_trailing._interpolatedObsTrackLB[0,0],
+            tsdf_trailing._interpolatedObsTrackLB[0,1],
+            degree=True)[1] < -0.1):
+            interpcs[ii]= origic+tries[nt]
+            nt+= 1
+        else:
+            ii+= 1
+            nt= 0
+            if ii < len(interpcs): origic= interpcs[ii]
+            # Add to the list
+            sdf_trailing_varyc.append(tsdf_trailing)
+            sdf_leading_varyc.append(tsdf_leading)
     # Compute the track properties for each model
     trackRADec_trailing= numpy.empty((len(interpcs),
                                       sdf_trailing_varyc[0]\
@@ -233,4 +215,191 @@ def predict_pal5obs(pot_params,c,
     length_out= ip(c)
     return (trackRADec_trailing_out,trackRADec_leading_out,
             trackRAVlos_trailing_out,trackRAVlos_leading_out,
-            width_out,length_out)
+            width_out,length_out,interpcs)
+
+def pal5_lnlike(pos_radec,rvel_ra,
+                trackRADec_trailing,trackRADec_leading,
+                trackRAVlos_trailing,trackRAVlos_leading,
+                width_out,length_out,interpcs): # last one so we can do *args
+    """
+    Returns array [nmodel,5] with log likelihood for each a) model, b) data set (trailing position, leading position, trailing vlos, actual width, actual length)
+    """
+    nmodel= trackRADec_trailing.shape[0]
+    out= numpy.zeros((nmodel,5))-1000000000000000.
+    for nn in range(nmodel):
+        # Interpolate trailing RA,Dec track
+        sindx= numpy.argsort(trackRADec_trailing[nn,:,0])
+        ipdec= \
+            interpolate.InterpolatedUnivariateSpline(\
+            trackRADec_trailing[nn,sindx,0],trackRADec_trailing[nn,sindx,1],
+            k=1) # to be on the safe side
+        tindx= pos_radec[:,0] > 229.
+        out[nn,0]= \
+            -0.5*numpy.sum((ipdec(pos_radec[tindx,0])-pos_radec[tindx,1])**2.\
+                              /pos_radec[tindx,2]**2.)
+        # Interpolate leading RA,Dec track
+        sindx= numpy.argsort(trackRADec_leading[nn,:,0])
+        ipdec= \
+            interpolate.InterpolatedUnivariateSpline(\
+            trackRADec_leading[nn,sindx,0],trackRADec_leading[nn,sindx,1],
+            k=1) # to be on the safe side
+        tindx= pos_radec[:,0] < 229.
+        out[nn,1]= \
+            -0.5*numpy.sum((ipdec(pos_radec[tindx,0])-pos_radec[tindx,1])**2.\
+                              /pos_radec[tindx,2]**2.)
+        # Interpolate trailing RA,Vlos track
+        sindx= numpy.argsort(trackRAVlos_trailing[nn,:,0])
+        ipvlos= \
+            interpolate.InterpolatedUnivariateSpline(\
+            trackRAVlos_trailing[nn,sindx,0],trackRAVlos_trailing[nn,sindx,1],
+            k=1) # to be on the safe side
+        tindx= rvel_ra[:,0] > 230.5
+        out[nn,2]= \
+            -0.5*numpy.sum((ipvlos(rvel_ra[tindx,0])-rvel_ra[tindx,1])**2.\
+                              /rvel_ra[tindx,2]**2.)
+        out[nn,3]= width_out[nn]
+        out[nn,4]= length_out[nn]
+    return out
+
+def setup_sdf(pot,prog,sigv,td,ro,vo,multi=None,nTrackChunks=8,isob=None):
+    if isob is None:
+        # Determine good one
+        ts= numpy.linspace(0.,150.,1001)
+        prog.integrate(ts,pot)
+        estb= estimateBIsochrone(pot,
+                                 prog.R(ts,use_physical=False),
+                                 prog.z(ts,use_physical=False))
+        if estb[1] < 0.3: isob= 0.3
+        elif estb[1] > 1.5: isob= 1.5
+        else: isob= estb[1]
+    print(pot[2]._c, isob)
+    aAI= actionAngleIsochroneApprox(pot=pot,b=isob)
+    try:
+        sdf_trailing=\
+            streamdf(sigv/vo,progenitor=prog,pot=pot,aA=aAI,
+                     leading=False,nTrackChunks=nTrackChunks,
+                     tdisrupt=td/bovy_conversion.time_in_Gyr(vo,ro),
+                     ro=ro,vo=vo,R0=ro,
+                     vsun=[-11.1,vo+24.,7.25],
+                     custom_transform=_TPAL5,
+                     multi=multi)
+    except numpy.linalg.LinAlgError:
+        sdf_trailing=\
+            streamdf(sigv/vo,progenitor=prog,pot=pot,aA=aAI,
+                     leading=False,nTrackChunks=nTrackChunks,
+                     nTrackIterations=0,
+                     tdisrupt=td/bovy_conversion.time_in_Gyr(vo,ro),
+                     ro=ro,vo=vo,R0=ro,
+                     vsun=[-11.1,vo+24.,7.25],
+                     custom_transform=_TPAL5,
+                     multi=multi)
+    try:
+        sdf_leading=\
+            streamdf(sigv/vo,progenitor=prog,pot=pot,aA=aAI,
+                     leading=True,nTrackChunks=nTrackChunks,
+                     tdisrupt=td/bovy_conversion.time_in_Gyr(vo,ro),
+                     ro=ro,vo=vo,R0=ro,
+                     vsun=[-11.1,vo+24.,7.25],
+                     custom_transform=_TPAL5,
+                     multi=multi)
+    except numpy.linalg.LinAlgError:
+        sdf_leading=\
+            streamdf(sigv/vo,progenitor=prog,pot=pot,aA=aAI,
+                     leading=True,nTrackChunks=nTrackChunks,
+                     nTrackIterations=0,
+                     tdisrupt=td/bovy_conversion.time_in_Gyr(vo,ro),
+                     ro=ro,vo=vo,R0=ro,
+                     vsun=[-11.1,vo+24.,7.25],
+                     custom_transform=_TPAL5,
+                     multi=multi)
+    return (sdf_trailing,sdf_leading)
+
+def pal5_data():
+    pos_radec= numpy.array([[241.48,6.41,0.09],
+                            [240.98,6.15,0.09],
+                            [240.48,6.20,0.09],
+                            [239.98,5.81,0.09],
+                            [239.48,5.64,0.09],
+                            [238.48,5.38,0.09],
+                            [237.98,5.14,0.09],
+                            [233.61,3.17,0.06],
+                            [233.11,2.88,0.06],
+                            [232.61,2.54,0.06],
+                            [232.11,2.23,0.06],
+                            [231.61,2.04,0.06],
+                            [231.11,1.56,0.06],
+                            [230.11,0.85,0.06],
+                            [229.11,0.54,0.06],
+                            [228.48,-0.77,0.11],
+                            [228.11,-1.16,0.14],
+                            [227.73,-1.28,0.11],
+                            [227.23,-2.03,0.17],
+                            [226.55,-2.59,0.14]])
+    rvel_ra= numpy.array([[225+15*15/60+48.19*0.25/60,-55.9,1.2],
+                          [225+15*15/60+49.70*0.25/60,-56.9,0.4],
+                          [225+15*15/60+52.60*0.25/60,-56.0,0.6],
+                          [225+15*15/60+54.79*0.25/60,-57.6,1.6],
+                          [225+15*15/60+56.11*0.25/60,-57.9,0.7],
+                          [225+15*15/60+57.05*0.25/60,-55.6,1.5],
+                          [225+15*15/60+58.26*0.25/60,-56.4,1.0],
+                          [225+15*15/60+58.89*0.25/60,-55.9,0.3],
+                          [225+15*15/60+59.52*0.25/60,-59.0,0.4],
+                          [225+16*15/60+02.00*0.25/60,-58.0,0.8],
+                          [225+16*15/60+03.61*0.25/60,-57.7,2.5],
+                          [225+16*15/60+04.81*0.25/60,-57.2,2.7],
+                          [225+16*15/60+06.54*0.25/60,-57.1,0.2],
+                          [225+16*15/60+07.75*0.25/60,-60.6,0.3],
+                          [225+16*15/60+08.51*0.25/60,-60.9,3.3],
+                          [225+16*15/60+19.83*0.25/60,-56.9,1.0],
+                          [225+16*15/60+23.11*0.25/60,-58.0,2.5],
+                          [225+16*15/60+34.71*0.25/60,-58.2,3.8],
+                          [225+16*15/60+08.66*0.25/60,-56.8,0.7],
+                          [225+16*15/60+09.58*0.25/60,-57.7,0.3],
+                          [225+15*15/60+52.84*0.25/60,-55.7,0.6],
+                          [225+15*15/60+56.21*0.25/60,-55.9,0.7],
+                          [225+16*15/60+05.26*0.25/60,-54.3,0.3],
+                          [225+17*15/60+09.99*0.25/60,-57.0,0.4],
+                          [225+17*15/60+34.55*0.25/60,-56.5,3.1],
+                          [225+17*15/60+58.32*0.25/60,-57.5,3.3],
+                          [225+18*15/60+04.96*0.25/60,-57.7,2.6],
+                          [225+18*15/60+18.92*0.25/60,-57.6,3.6],
+                          [225+18*15/60+35.89*0.25/60,-56.7,1.3],
+                          [225+19*15/60+21.42*0.25/60,-61.7,3.1],
+                          [225+21*15/60+51.16*0.25/60,-55.6,0.4],
+                          [225+24*15/60+04.85*0.25/60,-56.5,2.6],
+                          [225+24*15/60+13.00*0.25/60,-50.0,2.4],
+                          [225+28*15/60+39.20*0.25/60,-56.6,1.4],
+                          [225+28*15/60+49.34*0.25/60,-52.4,3.8],
+                          [225+34*15/60+19.31*0.25/60,-55.8,1.8],
+                          [225+34*15/60+31.90*0.25/60,-52.7,4.0],
+                          [225+34*15/60+56.51*0.25/60,-51.9,1.6],
+                          [225+45*15/60+10.57*0.25/60,-45.6,2.6],
+                          [225+46*15/60+49.44*0.25/60,-48.0,2.4],
+                          [225+48*15/60+57.99*0.25/60,-46.7,2.3],
+                          [225+55*15/60+24.13*0.25/60,-41.0,2.7],
+                          [240+0*15/60+45.41*0.25/60,-41.1,2.8],
+                          [240+1*15/60+12.59*0.25/60,-40.8,2.5],
+                          [240+3*15/60+29.59*0.25/60,-45.2,3.9],
+                          [240+4*15/60+05.53*0.25/60,-44.9,4.0],
+                          [240+4*15/60+33.28*0.25/60,-45.1,3.5],
+                          [240+13*15/60+40.97*0.25/60,-41.1,3.4],
+                          [240+16*15/60+44.79*0.25/60,-44.0,3.0],
+                          [240+16*15/60+51.73*0.25/60,-43.5,2.5],
+                          [225+8*15/60+07.15*0.25/60,-57.8,1.1],
+                          [225+8*15/60+17.50*0.25/60,-62.0,2.3],
+                          [225+10*15/60+39.02*0.25/60,-58.0,1.0],
+                          [225+11*15/60+09.04*0.25/60,-66.9,2.1],
+                          [225+11*15/60+21.70*0.25/60,-53.8,1.1],
+                          [225+12*15/60+45.44*0.25/60,-52.5,2.2],
+                          [225+13*15/60+40.44*0.25/60,-58.6,1.4],
+                          [225+13*15/60+54.40*0.25/60,-59.8,3.7],
+                          [225+14*15/60+09.32*0.25/60,-57.9,3.5],
+                          [225+14*15/60+17.18*0.25/60,-59.2,1.7],
+                          [225+14*15/60+20.71*0.25/60,-56.7,2.3],
+                          [225+14*15/60+34.63*0.25/60,-59.1,1.3],
+                          [225+15*15/60+16.47*0.25/60,-58.6,2.3],
+                          [225+15*15/60+50.43*0.25/60,-55.7,2.3],
+                          [225+16*15/60+01.54*0.25/60,-58.7,1.4],
+                          [225+16*15/60+34.95*0.25/60,-59.7,0.4],
+                          [225+16*15/60+56.20*0.25/60,-58.7,0.2]])
+    return (pos_radec,rvel_ra)
