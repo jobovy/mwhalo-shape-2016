77ed809742984ba3392013347e542c4eeb5c2332
Jo Bovy <bovy@astro.utoronto.ca>
2016-07-25 11:47:23 -0400

    Two modules for analyzing the Pal 5 observations

--- py/analyze_pal5.py
+++ py/analyze_pal5.py
@@ -8,6 +8,7 @@ from optparse import OptionParser
 import subprocess
 import numpy
 import pal5_util
+import MWPotential2014Likelihood
 _DATADIR= os.getenv('DATADIR')
 def get_options():
     usage = "usage: %prog [options]"
@@ -31,20 +32,42 @@ def get_options():
     # c grid
     parser.add_option("--cmin",dest='cmin',default=0.5,type='float',
                       help="Minimum c to consider")
-    parser.add_option("--cmax",dest='cmax',default=2.2,type='float',
+    parser.add_option("--cmax",dest='cmax',default=1.5,type='float',
                       help="Maximum c to consider")
     parser.add_option("--cstep",dest='cstep',default=0.01,type='float',
                       help="C resolution")
+    # Heuristic guess of the maximum
+    parser.add_option("--alongbfpm",action="store_true", 
+                      dest="alongbfpm",default=False,
+                      help="If set, move along the best-fit distance in the heuristic guess")
     # Distances grid
-    parser.add_option("--dmin",dest='dmin',default=23.,type='float',
-                      help="Minimum distance to consider")
-    parser.add_option("--dmax",dest='dmax',default=23.5,type='float',
-                      help="Maximum distance to consider")
-    parser.add_option("--dstep",dest='dstep',default=0.05,type='float',
+    parser.add_option("--dmin",dest='dmin',default=21.,type='float',
+                      help="Minimum distance to consider in guess")
+    parser.add_option("--dmax",dest='dmax',default=25.,type='float',
+                      help="Maximum distance to consider in guess")
+    parser.add_option("--dstep",dest='dstep',default=0.02,type='float',
                       help="Distance resolution")
+    parser.add_option("--dgridmin",dest='dgridmin',default=-6,type='int',
+                      help="Minimum distance to consider in the final grid, in units of dstep from guess")
+    parser.add_option("--dgridmax",dest='dgridmax',default=7,type='int',
+                      help="Maximum distance to consider in the final grid, in units of dstep from guess")
+    # PM offsets grid
+    parser.add_option("--pmmin",dest='pmmin',default=-0.02,type='float',
+                      help="Minimum proper motion offset to consider in guess")
+    parser.add_option("--pmmax",dest='pmmax',default=0.02,type='float',
+                      help="Maximum proper motion offset to consider in guess")
+    parser.add_option("--pmstep",dest='pmstep',default=0.001,type='float',
+                      help="Proper-motion offset resolution")
+    parser.add_option("--pmgridmin",dest='pmgridmin',default=-3,type='int',
+                      help="Minimum proper motion offset to consider in the final grid, in units of pmstep from guess")
+    parser.add_option("--pmgridmax",dest='pmgridmax',default=4,type='int',
+                      help="Maximum proper motion offset to consider in the final grid, in units of pmstep from guess")
     # Multi-processing
     parser.add_option("-m",dest='multi',default=8,type='int',
                       help="Number of CPUs to use for streamdf setup")
+    # Increase the uncertainties?
+    parser.add_option("--emult",dest='emult',default=1.,type='float',
+                      help="If set, multiply the uncertainties by this factor")
     # Output file
     parser.add_option("-o",dest='outfilename',
                       default=None,
@@ -70,10 +93,11 @@ def load_samples(options):
 
 def analyze_one_model(options,cs,pot_params,dist,pmra,pmdec):
     # First just compute the interpolation points, to adjust the width and length
-    interpcs=[0.5,0.75,1.,1.25,1.5,1.75,2.,2.25]
+    interpcs=[0.5,0.75,0.875,1.,1.125,1.25,1.5,1.75]#,2.,2.25]
     pal5varyc= pal5_util.predict_pal5obs(pot_params,interpcs,
                                          dist=dist,pmra=pmra,pmdec=pmdec,
                                          ro=options.ro,vo=options.vo,
+                                         interpk=1,
                                          multi=options.multi,interpcs=interpcs)
     if len(pal5varyc[6]) == 0:
         out= numpy.zeros((len(cs),5))-1000000000000000.
@@ -85,22 +109,18 @@ def analyze_one_model(options,cs,pot_params,dist,pmra,pmdec):
                                               dist=dist,pmra=pmra,pmdec=pmdec,
                                               ro=options.ro,vo=options.vo,
                                               multi=options.multi,
-                                              interpcs=pal5varyc[6],
+                                              interpk=1,
+                                              interpcs=interpcs,
                                               sigv=sigv,td=td)
     pos_radec, rvel_ra= pal5_util.pal5_data()
+    pos_radec[:,2]*= options.emult
+    rvel_ra[:,2]*= options.emult
     return pal5_util.pal5_lnlike(pos_radec,rvel_ra,*pal5varyc_like)
 
 if __name__ == '__main__':
     parser= get_options()
     options,args= parser.parse_args()
-    # Setup distance grid
-    ds= numpy.arange(options.dmin,options.dmax+options.dstep,options.dstep)
-    if ds[-1] > options.dmax+options.dstep/2.: ds= ds[:-1]
-    # Setup c grid
-    cs= numpy.arange(options.cmin,options.cmax+options.cstep,options.cstep)
-    if cs[-1] > options.cmax+options.cstep/2.: cs= cs[:-1]
-    # Before seed, so really random
-    pmerr= numpy.random.normal(size=len(ds))*0.186
+    # Set random seed
     numpy.random.seed(options.seed)
     # Load potential parameters
     if options.bf_b15:
@@ -110,37 +130,83 @@ if __name__ == '__main__':
         pot_samples= load_samples(options)
         rndindx= numpy.random.permutation(pot_samples.shape[1])[options.pindx]
         pot_params= pot_samples[:,rndindx]
-    # Sample proper motion within its uncertainty, but along the dir. of stream
-    # other comb. strongly ruled out by beginning of the stream
-    pmras=  -2.296+pmerr
-    pmdecs= -2.257+2.257/2.296*pmerr
+    print pot_params
+    if os.path.exists(options.outfilename):
+        # Read the distance/proper motion grid from the file
+        with open(options.outfilename,'rb') as savefile:
+            line1= savefile.readline()
+            cline= savefile.readline()
+            dline= savefile.readline()
+            dsline= savefile.readline()
+            pmsline= savefile.readline()
+        ds= numpy.array([float(d) for d in dsline[1:].split(',')])
+        pms= numpy.array([float(pm) for pm in pmsline[1:].split(',')])
+    else:
+        # Find good distances and proper motions
+        pot= MWPotential2014Likelihood.setup_potential(pot_params,1.,
+                                                       False,False,
+                                                       options.ro,options.vo)
+        dpmguess= pal5_util.pal5_dpmguess(pot,alongbfpm=options.alongbfpm,
+                                          pmmin=options.pmmin,
+                                          pmmax=options.pmmax,
+                                          pmstep=options.pmstep,
+                                          dmin=options.dmin,dmax=options.dmax,
+                                          vo=options.vo,ro=options.ro)
+        bestd= dpmguess[0]
+        bestpm= dpmguess[1]
+        print "Good distance and proper motion are: %.2f, %.2f" \
+            % (bestd,bestpm)
+        # March along the maximum line if alongbfpm
+        ds= []
+        pms= []
+        for ii in range(options.dgridmin,options.dgridmax):
+            for jj in range(options.pmgridmin,options.pmgridmax):
+                ds.append(bestd+ii*options.dstep)
+                pms.append(bestpm+(ds[-1]-bestd)*0.099+jj*options.pmstep)
+    pmdecpar= 2.257/2.296
+    # Setup c grid
+    cs= numpy.arange(options.cmin,options.cmax+options.cstep/2.,options.cstep)
+    if cs[-1] > options.cmax+options.cstep/2.: cs= cs[:-1]
     # Output
     if os.path.exists(options.outfilename):
         # Figure out how many ds were already run from the length of the file
         flen= filelen(options.outfilename)
-        start_lines= 3
+        start_lines= 5
         line_per_dist= 5
         ii= (flen-start_lines)//line_per_dist
         outfile= open(options.outfilename,'a')
     else:
         # Setup the file
         outfile= open(options.outfilename,'w')
-        outfile.write('# potparams:,%.8f,%.8f,%.8f,%.8f,%.8f\n' % \
+        outfile.write('# potparams:%.8f,%.8f,%.8f,%.8f,%.8f\n' % \
                           (pot_params[0],pot_params[1],pot_params[2],
                            pot_params[3],pot_params[4]))
         outfile.write('# cmin cmax cstep: %.3f,%.3f,%.3f\n' % \
                           (options.cmin,options.cmax,options.cstep))
         outfile.write('# dmin dmax dstep: %.3f,%.3f,%.3f\n' % \
-                          (options.dmin,options.dmax,options.dstep))
+                          (numpy.amin(ds),numpy.amax(ds),options.dstep))
+        nextline= '#'
+        for ii,d in enumerate(ds):
+            nextline+= '%.4f' % d
+            if not ii == len(ds)-1: nextline+= ','
+        outfile.write(nextline+'\n')
+        nextline= '#'
+        for ii,pm in enumerate(pms):
+            nextline+= '%.4f' % pm
+            if not ii == len(pms)-1: nextline+= ','
+        outfile.write(nextline+'\n')
         outfile.flush()
         ii= 0
     outwriter= csv.writer(outfile,delimiter=',')
     # Analyze each distance, pmra, pmdec
     while ii < len(ds):
+        dist= ds[ii]
+        pmo= pms[ii]
+        pmra= -2.296+pmo
+        pmdec= -2.257+pmo*pmdecpar
         print "Working on %i: dist %.2f, pmra %.3f, pmdec %.3f" \
-            % (ii,ds[ii],pmras[ii],pmdecs[ii])
-        likes= analyze_one_model(options,cs,pot_params,
-                                 ds[ii],pmras[ii],pmdecs[ii]).T
+            % (ii,dist,pmra,pmdec)
+        likes= analyze_one_model(options,cs,pot_params,dist,pmra,pmdec).T
         # Write
         for row in likes:
             outwriter.writerow(row)
---
+++ py/mcmc_pal5.py
@@ -0,0 +1,264 @@
+###############################################################################
+# mcmc_pal5.py: module to run MCMC analysis of the Pal 5 stream
+###############################################################################
+import os, os.path
+import copy
+import time
+import pickle
+import csv
+from optparse import OptionParser
+import subprocess
+import numpy
+from scipy.misc import logsumexp
+import emcee
+import pal5_util
+_DATADIR= os.getenv('DATADIR')
+def get_options():
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage=usage)
+    # Potential parameters
+    parser.add_option("--bf_b15",action="store_true", 
+                      dest="bf_b15",default=False,
+                      help="If set, use the best-fit to the MWPotential2014 data")
+    parser.add_option("--seed",dest='seed',default=1,type='int',
+                      help="seed for potential parameter selection and everything else")
+    parser.add_option("--fitsigma",action="store_true", 
+                      dest="fitsigma",default=False,
+                      help="If set, fit for the velocity-dispersion parameter")
+    parser.add_option("--dt",dest='dt',default=10.,type='float',
+                      help="Run MCMC for this many minutes")
+    parser.add_option("-i",dest='pindx',default=None,type='int',
+                      help="Index into the potential samples to consider")
+    parser.add_option("--ro",dest='ro',default=pal5_util._REFR0,type='float',
+                      help="Distance to the Galactic center in kpc")
+    parser.add_option("--td",dest='td',default=5.,type='float',
+                      help="Age of the stream")
+    parser.add_option("--samples_savefilename",
+                      dest='samples_savefilename',
+                      default='mwpot14varyc-samples.pkl',
+                      help="Name of the file that contains the potential samples")
+    # Output file
+    parser.add_option("-o",dest='outfilename',
+                      default=None,
+                      help="Name of the file that will hold the output")
+    # Multi-processing
+    parser.add_option("-m",dest='multi',default=1,type='int',
+                      help="Number of CPUs to use for streamdf setup")
+    return parser
+
+
+def filelen(filename):
+    p= subprocess.Popen(['wc','-l',filename],stdout=subprocess.PIPE, 
+                        stderr=subprocess.PIPE)
+    result,err = p.communicate()
+    if p.returncode != 0:
+        raise IOError(err)
+    return int(result.strip().split()[0])
+
+def load_samples(options):
+    if os.path.exists(options.samples_savefilename):
+        with open(options.samples_savefilename,'rb') as savefile:
+            s= pickle.load(savefile)
+    else:
+        raise IOError("File %s that is supposed to hold the potential samples does not exist" % options.samples_savefilename)
+    return s
+
+def find_starting_point(options,pot_params,dist,pmra,pmdec,sigv):
+    # First just compute the interpolation points, to adjust the width and length
+    interpcs=[0.65,0.75,0.875,1.,1.125,1.25,1.5,1.65]
+    cs= numpy.arange(0.7,1.61,0.01)
+    pal5varyc_like= pal5_util.predict_pal5obs(pot_params,cs,
+                                              dist=dist,pmra=pmra,pmdec=pmdec,
+                                              sigv=sigv,td=options.td,
+                                              ro=options.ro,vo=220.,
+                                              interpk=1,
+                                              interpcs=interpcs,
+                                              trailing_only=True,verbose=False)
+    pos_radec, rvel_ra= pal5_util.pal5_data()
+    if options.fitsigma:
+        lnlike= numpy.sum(\
+            pal5_util.pal5_lnlike(pos_radec,rvel_ra,
+                                  pal5varyc_like[0],
+                                  pal5varyc_like[1],
+                                  pal5varyc_like[2],
+                                  pal5varyc_like[3],
+                                  pal5varyc_like[4],
+                                  pal5varyc_like[5],
+                                  pal5varyc_like[6])[:,:3:2],axis=1)
+    else:
+        # For each one, move the track up and down a little to simulate sig changes
+        deco= numpy.linspace(-0.5,0.5,101)
+        lnlikes= numpy.zeros((len(cs),len(deco)))-100000000000000000.
+        for jj,do in enumerate(deco):
+            tra= pal5varyc_like[0]
+            tra[:,:,1]+= do
+            lnlikes[:,jj]= pal5_util.pal5_lnlike(pos_radec,rvel_ra,
+                                                 tra,pal5varyc_like[1],
+                                                 pal5varyc_like[2],
+                                                 pal5varyc_like[3],
+                                                 pal5varyc_like[4],
+                                                 pal5varyc_like[5],
+                                                 pal5varyc_like[6])[:,0]
+        lnlike= numpy.amax(lnlikes,axis=1)
+    return cs[numpy.argmax(lnlike)]
+
+def lnp(p,pot_params,options):
+    #p=[c,vo/220,dist/22.,pmo_parallel,pmo_perp] and ln(sigv) if fitsigma
+    c= p[0]
+    vo= p[1]*pal5_util._REFV0
+    dist= p[2]*22.
+    pmra= -2.296+p[3]+p[4]
+    pmdecpar= 2.257/2.296
+    pmdecperp= -2.296/2.257
+    pmdec= -2.257+p[3]*pmdecpar+p[4]*pmdecperp
+    if options.fitsigma:
+        sigv= 0.4*numpy.exp(p[5])
+    else:
+        sigv= 0.4
+    # Priors
+    if c < 0.5: return -100000000000000000.
+    elif c > 2.: return -10000000000000000.
+    elif vo < 200: return -10000000000000000.
+    elif vo > 250: return -10000000000000000.
+    elif dist < 19.: return -10000000000000000.
+    elif dist > 24.: return -10000000000000000.
+    elif options.fitsigma and sigv < 0.1: return -10000000000000000.
+    elif options.fitsigma and sigv > 1.: return -10000000000000000.
+    # Setup the model
+    pal5varyc_like= pal5_util.predict_pal5obs(pot_params,c,singlec=True,
+                                              dist=dist,pmra=pmra,pmdec=pmdec,
+                                              ro=options.ro,vo=vo,
+                                              trailing_only=True,verbose=False,
+                                              sigv=sigv,td=options.td,
+                                              nTrackChunks=21)
+    pos_radec, rvel_ra= pal5_util.pal5_data()
+    if options.fitsigma:
+        lnlike= pal5_util.pal5_lnlike(pos_radec,rvel_ra,
+                                      pal5varyc_like[0],
+                                      pal5varyc_like[1],
+                                      pal5varyc_like[2],
+                                      pal5varyc_like[3],
+                                      pal5varyc_like[4],
+                                      pal5varyc_like[5],
+                                      pal5varyc_like[6])
+        return lnlike[0,0]+lnlike[0,2]\
+            -0.5*(pmra+2.296)**2./0.186**2.-0.5*(pmdec+2.257)**2./0.181**2.
+    # If not fitsigma, move the track up and down a little to simulate sig changes
+    deco= numpy.linspace(-0.5,0.5,101)
+    lnlikes= numpy.zeros(len(deco))-100000000000000000.
+    for jj,do in enumerate(deco):
+        tra= copy.deepcopy(pal5varyc_like[0])
+        tra[:,:,1]+= do
+        lnlikes[jj]= pal5_util.pal5_lnlike(pos_radec,rvel_ra,
+                                           tra,pal5varyc_like[1],
+                                           pal5varyc_like[2],
+                                           pal5varyc_like[3],
+                                           pal5varyc_like[4],
+                                           pal5varyc_like[5],
+                                           pal5varyc_like[6])[0,0]
+    return logsumexp(lnlikes)\
+        +pal5_util.pal5_lnlike(pos_radec,rvel_ra,
+                               pal5varyc_like[0],pal5varyc_like[1],
+                               pal5varyc_like[2],pal5varyc_like[3],
+                               pal5varyc_like[4],pal5varyc_like[5],
+                               pal5varyc_like[6])[0,2]\
+        -0.5*(pmra+2.296)**2./0.186**2.-0.5*(pmdec+2.257)**2./0.181**2.
+
+if __name__ == '__main__':
+    parser= get_options()
+    options,args= parser.parse_args()
+    # Set random seed
+    numpy.random.seed(options.seed)
+    # Load potential parameters
+    if options.bf_b15:
+        pot_params= [0.60122692,0.36273147,-0.97591502,-3.34169377,
+                     0.71877924,-0.01519337,-0.01928001]
+    else:
+        pot_samples= load_samples(options)
+        rndindx= numpy.random.permutation(pot_samples.shape[1])[options.pindx]
+        pot_params= pot_samples[:,rndindx]
+    print pot_params
+    lnp_func= lambda p: lnp(p,pot_params,options)
+    nwalkers= 10+2*options.fitsigma
+    # For a fiducial set of parameters, find a good fit to use as the starting 
+    # point
+    all_start_params= numpy.zeros((nwalkers,5+options.fitsigma))
+    start_lnprob0= numpy.zeros(nwalkers)
+    if not os.path.exists(options.outfilename):
+        pmra= -2.296
+        pmdec= -2.257
+        dist= 23.2
+        cstart= find_starting_point(options,pot_params,dist,pmra,pmdec,0.4)
+        if options.fitsigma:
+            start_params= numpy.array([cstart,1.,dist/22.,0.,0.,0.])
+            step= numpy.array([0.05,0.05,0.05,0.05,0.01,0.05])
+        else:
+            start_params= numpy.array([cstart,1.,dist/22.,0.,0.])
+            step= numpy.array([0.05,0.05,0.05,0.05,0.01])
+        nn= 0
+        while nn < nwalkers:
+            all_start_params[nn]= start_params\
+                +numpy.random.normal(size=len(start_params))*step
+            start_lnprob0[nn]= lnp_func(all_start_params[nn])
+#            if start_lnprob0[nn] > -1000000.: 
+#                print all_start_params[nn], start_lnprob0[nn]
+            if start_lnprob0[nn] > -1000000.: nn+= 1
+    else:
+        # Get the starting point from the output file
+        with open(options.outfilename,'rb') as savefile:
+            all_lines= savefile.readlines()
+        for nn in range(nwalkers):
+            lastline= all_lines[-1-nn]
+            tstart_params= numpy.array([float(s) for s in lastline.split(',')])
+            start_lnprob0[nn]= tstart_params[-1]
+            all_start_params[nn]= tstart_params[:-1]
+    # Output
+    if os.path.exists(options.outfilename):
+        outfile= open(options.outfilename,'a',0)
+    else:
+        # Setup the file
+        outfile= open(options.outfilename,'w',0)
+        outfile.write('# potparams:%.8f,%.8f,%.8f,%.8f,%.8f\n' % \
+                          (pot_params[0],pot_params[1],pot_params[2],
+                           pot_params[3],pot_params[4]))
+        for nn in range(nwalkers):
+            if options.fitsigma:
+                outfile.write('%.8f,%.8f,%.8f,%.8f,%.8f,%.8f,%.8f\n' % \
+                                  (all_start_params[nn,0],all_start_params[nn,1],
+                                   all_start_params[nn,2],all_start_params[nn,3],
+                                   all_start_params[nn,4],all_start_params[nn,5],
+                                   start_lnprob0[nn]))
+            else:
+                outfile.write('%.8f,%.8f,%.8f,%.8f,%.8f,%.8f\n' % \
+                                  (all_start_params[nn,0],all_start_params[nn,1],
+                                   all_start_params[nn,2],all_start_params[nn,3],
+                                   all_start_params[nn,4],start_lnprob0[nn]))
+        outfile.flush()
+    outwriter= csv.writer(outfile,delimiter=',')
+    # Run MCMC
+    sampler= emcee.EnsembleSampler(nwalkers,all_start_params.shape[1],lnp_func,
+                                   threads=options.multi)
+    rstate0= numpy.random.mtrand.RandomState().get_state()
+    start= time.time()
+    while time.time() < start+options.dt*60.:
+        new_params, new_lnp, new_rstate0=\
+            sampler.run_mcmc(all_start_params,1,lnprob0=start_lnprob0,
+                             rstate0=rstate0,storechain=False)
+        all_start_params= new_params
+        start_lnprob0= new_lnp
+        rstate0= new_rstate0
+        for nn in range(nwalkers):
+            if options.fitsigma:
+                outfile.write('%.8f,%.8f,%.8f,%.8f,%.8f,%.8f,%.8f\n' % \
+                                  (all_start_params[nn,0],all_start_params[nn,1],
+                                   all_start_params[nn,2],all_start_params[nn,3],
+                                   all_start_params[nn,4],all_start_params[nn,5],
+                                   start_lnprob0[nn]))
+            else:
+                outfile.write('%.8f,%.8f,%.8f,%.8f,%.8f,%.8f\n' % \
+                                  (all_start_params[nn,0],all_start_params[nn,1],
+                                   all_start_params[nn,2],all_start_params[nn,3],
+                                   all_start_params[nn,4],start_lnprob0[nn]))
+        outfile.flush()
+    outfile.close()            
+    
